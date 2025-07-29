e8c5bd19c9b652559511010045c1076d49fa23d2
pkbceira03 <cabeceira2003@gmail.com>
2025-07-29 18:48:39 -0300

    feat: adiciona exemplos com strings

---
+++ javascript/string.js
@@ -0,0 +1,52 @@
+// function ucFirst(str){
+//     var final = str[0].toUpperCase()+str.slice(1)
+//     console.log(final)
+// }
+
+// ucFirst("pedro")
+
+
+
+
+// function checkSpam(str){
+//     let strNew = str.toUpperCase()
+
+//     console.log(str)
+
+//     if(strNew.includes("XXX")){
+//         console.log(strNew.indexOf("XXX"))
+//         return true
+//     }else if(strNew.includes("VIAGRA")){
+//         console.log(strNew.indexOf("VIAGRA"))
+//         return true
+//     }else{
+//         console.log(false)
+//         return false
+//     }
+// }
+
+// checkSpam('buy ViAgRA now') 
+// checkSpam('free xxxxx')
+// checkSpam("innocent rabbit") 
+
+
+
+
+// function truncate(str, maxlength){
+//     if(str.length> maxlength){
+//         console.log(str.substr(0,19)+'...')
+//     }else{
+//         console.log(str)
+//     }
+// }
+
+// truncate("What I'd like to tell on this topic is:", 20)
+
+// truncate("Hi everyone!", 20)
+
+
+// function extractCurrencyValue(str){
+//     console.log(str.slice(1))
+// }
+
+// extractCurrencyValue('$120')
