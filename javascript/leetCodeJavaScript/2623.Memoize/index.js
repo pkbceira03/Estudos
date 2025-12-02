7e320861ec07cc14a3a3fc61fb5e75b4e2efff69
pkbceira03 <cabeceira2003@gmail.com>
2025-12-02 10:43:14 -0300

    fix: memoize

--- javascript/leetCodeJavaScript/2623.Memoize/index.js
+++ javascript/leetCodeJavaScript/2623.Memoize/index.js
@@ -1,7 +1,7 @@
 function memoize(fn) {
     let aux = new Map()
     return function(...args) {
-        //const key = args.join(',');
+        const key = args.join(',');
         if(!aux.has(key)){
             const result = fn(...args);
             aux.set(key, result)
@@ -10,21 +10,6 @@ function memoize(fn) {
         }else{
             //console.log('tem')
             return aux.get(key);
-        }
-    }
-}
-
-function memoize(fn) {
-    const map = new Map();
-
-    return function(...args) {
-        const key = JSON.stringify(args);
-        if (map.has(key)) {
-            return map.get(key);
-        } else {
-            const fnResult = fn(...args);
-            map.set(key, fnResult);
-            return fnResult;
         }
     }
 }
