2c9783d015a672616e9fc7319eef5ecde0b666b6
pkbceira03 <cabeceira2003@gmail.com>
2025-12-02 17:16:02 -0300

    feat: adidicona funções na lista

--- javascript/queue.js
+++ javascript/queue.js
@@ -27,41 +27,63 @@ class Queue{
         this.size = 0;
     }
 
+    // adicionar na lista
     enqueue(val){
-        const newNode = new QueueNode(val)
+        const node = new QueueNode(val);
+
         if(this.size === 0){
-            this.front = newNode;
-            this.back = newNode;
+            this.front = node;
+            this.back = node;
         }else{
-            this.back = newNode;
-            this.back.next = newNode;
+            //ligamos o atual back ao novo
+            this.back.next = node;
+            this.back = node;
         }
 
-        this.size++;
+        this.size ++;
     }
 
+    //remove da lista
     dequeue(){
-        if(tjis.size === 0) return null;
+        if(this.size === 0)return null;
 
-        const removedNode = this.front;
-        
+        const removedFront = this.front;
         if(this.size === 1){
-            this.back = null
+            this.back = null;
         }
 
+        //atuyalizamos o front com o novo
         this.front = this.front.next;
+
         this.size--;
-        return removedNode.val;
+
+        return removedFront.val;
+    }
+
+    getFirst(){
+        if(!this.front) return null;
+        return this.front.val;
+    }
+
+    getLast(){
+        if(!this.back) return null;
+        return this.back.val;
+    }
+
+    getSize(){
+        return this.size;
     }
 }
 
-const myQueue = new Queue
+const myQueue = new Queue;
 
-myQueue.enqueue('a');
-myQueue.enqueue('b');
-myQueue.enqueue('c');
+myQueue.enqueue(3);
+myQueue.enqueue(2);
+myQueue.enqueue(2);
+myQueue.enqueue(4);
+myQueue.enqueue(4);
+myQueue.enqueue(7);
+myQueue.dequeue();
 
-console.log(myQueue.size)
+console.log(myQueue.getFirst(),myQueue.getLast(),myQueue.getSize())
 
-console.log(myQueue.dequeue());
-console.log(myQueue.size)
